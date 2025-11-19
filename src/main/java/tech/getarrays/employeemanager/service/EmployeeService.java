f2831cf800a9ff6501533f6d04a9a67cf4959e35
veslino-marc <veslinomarc4@gmail.com>
2025-11-19 14:37:33 +0800

    Upload files for employeemanagement

---
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,48 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.EmployeemanagerApplication;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repository.EmployeeRepo;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    private EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by ID " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        if (!employeeRepo.existsById(id)) {
+            throw new UserNotFoundException("User with ID " + id + " was not found");
+        }
+
+        employeeRepo.deleteById(id);
+    }
+
+}
